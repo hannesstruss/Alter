926e48fea83e2601395adacab88d2b2c3cfbe6c4
Hannes Struß <x@hannesstruss.de>
2019-06-01 13:57:22 +0200

    ViewModel/Fragment work.

--- app/src/main/java/de/hannesstruss/alter/MainActivity.kt
+++ app/src/main/java/de/hannesstruss/alter/MainActivity.kt
@@ -2,11 +2,24 @@ package de.hannesstruss.alter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import shronq.statemachine.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+  }
+
+  override fun getSystemService(name: String): Any {
+    if (name == ViewModelFactory.SERVICE_NAME) {
+      return object : ViewModelFactory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+          return MainViewModel() as T
+        }
+      }
+    }
+    return super.getSystemService(name)
   }
 }
--- app/src/main/java/de/hannesstruss/alter/MainFragment.kt
+++ app/src/main/java/de/hannesstruss/alter/MainFragment.kt
@@ -2,18 +2,30 @@ package de.hannesstruss.alter
 
 import android.widget.TextView
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowViaChannel
 import shronq.statemachine.StateMachineFragment
 
 class MainFragment : StateMachineFragment<HelloState, HelloEvent, MainViewModel>() {
   override val layout = R.layout.fragment_main
   override val viewModelClass = MainViewModel::class.java
 
+  val txtHello get() = requireView().findViewById<TextView>(R.id.txt_hello)
+
   override fun events(): Flow<HelloEvent> {
-    return flow {}
+    val helloClicks = flowViaChannel<HelloEvent> { channel ->
+      val txt = txtHello
+      txt.setOnClickListener {
+        channel.offer(HelloEvent.Click)
+      }
+      channel.invokeOnClose {
+        txt.setOnClickListener(null)
+      }
+    }
+
+    return helloClicks
   }
 
   override fun render(state: HelloState) {
-    requireView().findViewById<TextView>(R.id.txt_hello)
+    txtHello.text = state.counter.toString()
   }
 }
