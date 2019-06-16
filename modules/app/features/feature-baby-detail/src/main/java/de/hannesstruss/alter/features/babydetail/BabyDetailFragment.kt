b43807314b408ec83cab3644a4a03fa6f628a93e
Hannes Struß <x@hannesstruss.de>
2019-06-16 09:36:38 +0200

    Format dob and due date

--- modules/app/features/feature-baby-detail/src/main/java/de/hannesstruss/alter/features/babydetail/BabyDetailFragment.kt
+++ modules/app/features/feature-baby-detail/src/main/java/de/hannesstruss/alter/features/babydetail/BabyDetailFragment.kt
@@ -11,10 +11,13 @@ import flowbinding.clicks
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import shronq.statemachine.FeatureComponent
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class BabyDetailFragment :
   FeatureDependencyProvidingFragment<BabyDetailState, BabyDetailEvent, BabyDetailViewModel>() {
   private val args: BabyDetailFragmentArgs by navArgs()
+  private val dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
 
   override val layout = R.layout.baby_detail_fragment
   override val viewModelClass = BabyDetailViewModel::class.java
@@ -27,9 +30,18 @@ class BabyDetailFragment :
   override fun render(state: BabyDetailState) {
     if (state.baby != null) {
       with(requireView()) {
+        val dob = state.baby.born_at?.let {
+          dateFormatter.format(it.toLocalDate())
+        } ?: ""
+        findViewById<TextView>(R.id.txt_dob).text = dob
+
+        val due = state.baby.due_on?.let {
+          dateFormatter.format(it)
+        }?: ""
+        findViewById<TextView>(R.id.txt_due_date).text = due
+
         findViewById<TextView>(R.id.txt_name).text = state.baby.name
         findViewById<TextView>(R.id.txt_parents).text = state.baby.parents
-        findViewById<TextView>(R.id.txt_dob).text = state.baby.born_at?.toString() ?: "No DOB"
         findViewById<TextView>(R.id.txt_age).text = state.age?.format(
           context = context,
           lessSignificantValueMode = LessSignificantValueMode.IfNonZero,
