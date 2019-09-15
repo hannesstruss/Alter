db6918e1521bf31cdc9c6db63f63b5bcee537176
Hannes Struß <x@hannesstruss.de>
2019-09-15 09:20:55 +0200

    Fix env var name

--- .github/workflows/setup_ci_env.sh
+++ .github/workflows/setup_ci_env.sh
@@ -3,11 +3,9 @@
 mkdir -p $HOME/.gradle
 cat <<EOF >> $HOME/.gradle/gradle.properties
 alter.upload_key_password=$ALTER_UPLOAD_KEY_PASSWORD
-alter.upload_store_password=$ALTER_UPLOAD_STORE_PASSWOD
+alter.upload_store_password=$ALTER_UPLOAD_STORE_PASSWORD
 alter.bugsnag_key=$ALTER_BUGSNAG_KEY
 EOF
-
-cat $HOME/.gradle/gradle.properties
 
 echo "$ALTER_PUBLISHKEY_JSON_B64" | base64 -d > app/publishkey.json
 echo "$ALTER_UPLOAD_KEY_B64" | base64 -d > app/upload.keystore
