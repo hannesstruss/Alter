52a09ff2ea24336f842fdfc4c88a8eabe3017037
Hannes Struß <x@hannesstruss.de>
2019-09-15 09:00:18 +0200

    Setup CI Env and push a build

---
+++ .github/workflows/setup_ci_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p $HOME/.gradle
+cat <<EOF >> $HOME/.gradle/gradle.properties
+alter.upload_key_password=$ALTER_UPLOAD_KEY_PASSWORD
+alter.upload_store_password=$ALTER_UPLOAD_STORE_PASSWOD
+alter.bugsnag_key=$ALTER_BUGSNAG_KEY
+EOF
+
+echo "$ALTER_PUBLISHKEY_JSON_B64" | base64 -d > app/publishkey.json
+echo "$ALTER_UPLOAD_KEY_B64" | base64 -d > app/upload.keystore
