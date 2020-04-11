eb4c10eae5f956401669926de77812f4cac45584
Hannes Struß <x@hannesstruss.de>
2020-04-11 14:36:35 +0200

    Fix multiplatform tests

--- modules/app/db/src/commonTest/kotlin/de/hannesstruss/alter/db/adapters/DateAdapterTest.kt
+++ modules/app/db/src/commonTest/kotlin/de/hannesstruss/alter/db/adapters/DateAdapterTest.kt
@@ -1,20 +1,22 @@
 package de.hannesstruss.alter.db.adapters
 
 import de.hannesstruss.alter.dates.Date
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class DateAdapterTest {
   @Test fun decode() {
     val str = "2019-05-03"
-    assertThat(DateAdapter.decode(str)).isEqualTo(Date(2019, 5, 3))
+    assertEquals(Date(2019, 5, 3), DateAdapter.decode(str))
   }
 
   @Test fun encode() {
     val date = Date(2019, 5, 3)
-    assertThat(DateAdapter.encode(date)).isEqualTo("2019-05-03")
+    assertEquals("2019-05-03", DateAdapter.encode(date))
   }
 
   @Test fun `encode year with leading zeros`() {
     val date = Date(400, 5, 3)
-    assertThat(DateAdapter.encode(date)).isEqualTo("0400-05-03")
+    assertEquals("0400-05-03", DateAdapter.encode(date))
   }
 }
